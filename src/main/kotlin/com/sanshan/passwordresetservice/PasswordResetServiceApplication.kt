d1e5d7919d341ba10e0ffa0c26c79c4e1d235dd0
Sanshan Gan <sanshan.gan@thoughtworks.com>
2025-11-10 22:17:20 +0800

    Initial commit: Spring Boot Kotlin project structure

---
+++ src/main/kotlin/com/sanshan/passwordresetservice/PasswordResetServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sanshan.passwordresetservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PasswordResetServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PasswordResetServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sanshan/passwordresetservice/PasswordResetServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sanshan.passwordresetservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PasswordResetServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
